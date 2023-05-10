e9ce12d81317d7080d8a2a30de260f5c6bb3b9f7
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-05-10 03:16:03 +0300

    add the initial views to the apps

--- Tracker/views.py
+++ Tracker/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
 
 # Create your views here.
+
+
+
+
+class LecturViewSet(viewsets.ModelViewSet):
+    queryset = Lecture.objects.all()
+    serializer_class = LecturSerializer
+
+
+
+class AttendanceViewSet(viewsets.ModelViewSet):
+    queryset = Attendance.objects.all()
+    serializer_class = AttendanceSerializer
+    
